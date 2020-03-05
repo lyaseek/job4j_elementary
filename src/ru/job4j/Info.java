9bbb94fb85c309e1d0e92e39788df11c7b0fb71f
lyaseek <lyaseek@gmail.com>
2020-03-05 17:22:29 +0100

    3.4. Заголовок в файле java[#250057]

---
+++ src/ru/job4j/Info.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+import javax.xml.crypto.Data;
+import java.util.Date;
+
+public class Info {
+    public static void main(String[] args) {
+        System.out.println("05.03.2020");
+    }
+}
