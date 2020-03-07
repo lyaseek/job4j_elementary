90c7bd77912067e8c2ee4eeaf1319e8682954fcc
lyaseek <lyaseek@gmail.com>
2020-03-07 20:56:08 +0100

    4.1.2. Глупый бот.[#249997]

---
+++ src/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+public class DummyBot {
+
+    public static String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)){
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(answer("Пока!"));
+    }
+}
