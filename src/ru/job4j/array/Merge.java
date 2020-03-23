457930c1da392fb3daf14a126f2adf81b4e0eef0
lyaseek <lyaseek@gmail.com>
2020-03-23 11:24:16 +0100

    Объединить два массива [#124482]

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -6,37 +6,34 @@ import java.util.Arrays;
 public class Merge {
     public int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-        int leftIndex = 0, rightIndex = 0, i = 0;
+        int leftIndex = 0, rightIndex = 0;
 
-        if ((left.length == 0) && (right.length != 0)) {
+        if (left.length == 0) {
             for (int j = 0; j < right.length; j++) {
                 rsl[j] = right[j];
             }
             return rsl;
         }
-        if ((right.length == 0) && (left.length != 0)) {
+        if (right.length == 0) {
             for (int j = 0; j < left.length; j++) {
                 rsl[j] = left[j];
             }
             return rsl;
         }
-        while ((leftIndex + rightIndex) != rsl.length) {
+        while ((leftIndex < left.length) && (rightIndex < right.length)) {
             if (left[leftIndex] < right[rightIndex]) {
-                rsl[leftIndex + rightIndex] = left[leftIndex];
-                leftIndex++;
-                if (leftIndex == left.length) {
+                rsl[leftIndex + rightIndex] = left[leftIndex++];
+                if (leftIndex > left.length - 1) {
                     while (rightIndex < right.length) {
-                        rsl[rightIndex + leftIndex] = right[rightIndex];
-                        rightIndex++;
+                        rsl[rightIndex + leftIndex] = right[rightIndex++];
                     }
                 }
+
             } else {
-                rsl[leftIndex + rightIndex] = right[rightIndex];
-                rightIndex++;
-                if (rightIndex == right.length) {
+                rsl[leftIndex + rightIndex] = right[rightIndex++];
+                if (rightIndex > right.length - 1) {
                     while (leftIndex < left.length) {
-                        rsl[rightIndex + leftIndex] = left[leftIndex];
-                        leftIndex++;
+                        rsl[rightIndex + leftIndex] = left[leftIndex++];
                     }
                 }
             }
@@ -48,7 +45,7 @@ public class Merge {
         Merge process = new Merge();
         int[] rsl = process.merge(
                 new int[]{1, 2},
-                new int[]{3, 4, 4});
+                new int[]{3, 4, 5});
         System.out.println(Arrays.toString(rsl));
     }
 }
