f5b531e7c2b25d4e382ac8a046fd6bbc670583e3
lyaseek <lyaseek@gmail.com>
2020-04-01 12:58:54 +0200

    4. Переопределение[#250193]

--- src/ru/job4j/inheritance/ReportUsage.java
+++ src/ru/job4j/inheritance/ReportUsage.java
@@ -4,5 +4,7 @@ public class ReportUsage {
     public static void main(String[] args) {
         JSONReport report = new JSONReport();
         System.out.println(report.generate("name", "body"));
+        HtmlReport htmlReport = new HtmlReport();
+        System.out.println(htmlReport.generate("name", "body"));
     }
 }
