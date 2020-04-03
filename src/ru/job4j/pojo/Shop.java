6b5e02d19fb72eb9eeb754d11a561b4d36875d94
lyaseek <lyaseek@gmail.com>
2020-04-03 10:24:20 +0200

    3. Удаление моделей из массива.[#250201]

---
+++ src/ru/job4j/pojo/Shop.java
@@ -0,0 +1,69 @@
+package ru.job4j.pojo;
+
+import ru.job4j.loop.PrimeNumber;
+
+public class Shop {
+    public static void main(String[] args) {
+        Product[] products = new Product[5];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        products[2] = new Product("Egg", 19);
+
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        System.out.println();
+        System.out.println("Удаляем значение из ячейки с индексом 1");
+        products[1] = null;
+
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        System.out.println();
+        System.out.println("Записываем  в ячейку с индексом 1 значение ячейки с индексом 2 и удаляем значение из ячейки с индексом 2");
+        products[1] = products[2];
+        products[2] = null;
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+        System.out.println();
+        System.out.println("My task:");
+        products[3] = new Product("Fish", 5);
+        products[4] = new Product("Sugar", 1);
+        Shop shop = new Shop();
+        shop.delete(products, 2);
+        for (Product x : products) {
+            if (x != null) {
+                System.out.println(x.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+    }
+
+    public Product[] delete(Product[] products, int index) {
+        for (int i = index; i < products.length - 1; i++) {
+            products[i] = products[i + 1];
+        }
+        products[products.length - 1] = null;
+        return products;
+    }
+}
+
