3c6a4684f4477a66a6c1d85b7fbaa5608494f8ce
lyaseek <lyaseek@gmail.com>
2020-04-20 11:23:45 +0200

    0.3. Тестирование исключений с junit[#250160]

--- test/ru/job4j/ex/FactTest.java
+++ test/ru/job4j/ex/FactTest.java
@@ -2,9 +2,18 @@ package ru.job4j.ex;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.*;
+
 public class FactTest {
     @Test(expected = IllegalArgumentException.class)
     public void whenNIsLessThan0() {
         new Fact().calc(-4);
     }
+    @Test
+    public void whenNIsGreaterThan0() {
+        int rsl = new Fact().calc(6);
+        assertThat(rsl, is(720));
+    }
+
 }
