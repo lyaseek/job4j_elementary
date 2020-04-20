bb1aad825640427bdfca8be7aea265791d0e8693
lyaseek <lyaseek@gmail.com>
2020-04-20 12:23:35 +0200

    0.4. Пользовательские исключения.[#250161]

---
+++ test/ru/job4j/ex/FindEl.java
@@ -0,0 +1,28 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+                break;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("In String[] value there is no element equal to the key.");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] value = new String[]{
+                "apple", "orange", "plum"
+        };
+        try {
+            System.out.println("Index = " + FindEl.indexOf(value, "pineapple"));
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
