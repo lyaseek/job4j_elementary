47c4ef8088d3ecc6757ed8d90763fa400e5c5783
lyaseek <lyaseek@gmail.com>
2020-05-02 12:13:37 +0200

    1. Телефонный справочник на базе ArrayList[#250110]

---
+++ src/search/PhoneDictionary.java
@@ -0,0 +1,22 @@
+package search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<Person>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person x : persons) {
+            if ((x.getAddress().contains(key)) || (x.getName().contains(key)) || (x.getSurname().contains(key)) || (x.getPhone().contains(key))) {
+                result.add(x);
+            }
+        }
+        return result;
+    }
+
+}
