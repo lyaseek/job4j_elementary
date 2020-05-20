ca61ffd7487f39b27f80868bb782b82a965354d5
lyaseek <lyaseek@gmail.com>
2020-05-20 09:51:15 +0200

    1. Анонимные классы[214142#249972]

---
+++ src/ru/job4j/lambda/task249972/AttachmentSort.java
@@ -0,0 +1,31 @@
+package ru.job4j.lambda.task249972;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class AttachmentSort {
+    public static void main(String[] args) {
+        List<Attachment> attachments = Arrays.asList(
+                new Attachment("image 1", 100),
+                new Attachment("image 3", 34),
+                new Attachment("image 2", 13)
+        );
+        Comparator<Attachment> comparatorSize = new Comparator<Attachment>() {
+            @Override
+            public int compare(Attachment o1, Attachment o2) {
+                return o1.getSize() - o2.getSize();
+            }
+        };
+        attachments.sort(comparatorSize);
+        System.out.println(attachments);
+        Comparator<Attachment> comparatorName = new Comparator<Attachment>() {
+            @Override
+            public int compare(Attachment o1, Attachment o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        };
+        attachments.sort(comparatorName);
+        System.out.println(attachments);
+    }
+}
