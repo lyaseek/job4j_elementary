d5966c9e04f8d26e854503179ca699c20c254520
lyaseek <lyaseek@gmail.com>
2020-05-24 11:56:26 +0200

    2.0. Встроенные функциональные интерфейсы.[249209#249974]

--- src/ru/job4j/lambda/task249209/SearchAtt.java
+++ src/ru/job4j/lambda/task249209/SearchAtt.java
@@ -3,23 +3,28 @@ package ru.job4j.lambda.task249209;
 import ru.job4j.lambda.task249972.Attachment;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class SearchAtt {
-    public static List<Attachment> filter(List<Attachment> list, Object o) {
-        Supplier<List<Attachment>> filterList = new Supplier<List<Attachment>>() {
-            @Override
-            public List<Attachment> get() {
-                List<Attachment> rsl = new ArrayList<>();
-                for (Attachment x : list) {
-                    if (o instanceof String ? (x.getName().contains((String) o)) : x.getSize() > (Integer) o) {
-                        rsl.add(x);
-                    }
-                }
-                return rsl;
+    public static List<Attachment> filter(List<Attachment> list, Predicate<Attachment> filter) {
+        List<Attachment> rsl = new ArrayList<>();
+        for (Attachment att : list) {
+            if (filter.test(att)) {
+                rsl.add(att);
             }
-        };
-        return filterList.get();
+        }
+        System.out.println(rsl);
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        List<Attachment> list = Arrays.asList(new Attachment("Abug", 120), new Attachment("Bbb", 30), new Attachment("Nnbugd", 100));
+        Predicate<Attachment> containsBug = t -> t.getName().contains("bug");
+        Predicate<Attachment> moreThan100 = t -> t.getSize() > 100;
+        SearchAtt.filter(list, containsBug);
+        SearchAtt.filter(list, moreThan100);
     }
 }
