cfeaeb34c1db92251b294b714250817b5eb0a6ea
lyaseek <lyaseek@gmail.com>
2020-05-24 15:52:44 +0200

    6. Функции высшего порядка[24255#249968]

---
+++ src/ru/job4j/lambda/task249968/PhoneDictionary.java
@@ -0,0 +1,23 @@
+package ru.job4j.lambda.task249968;
+
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<Person>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    public ArrayList<Person> find(String key) {
+        Predicate<Person> combine = x -> x.getAddress().contains(key) || x.getName().contains(key) || x.getPhone().contains(key) || x.getSurname().contains(key);
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person person : persons) {
+            if (combine.test(person)) {
+                result.add(person);
+            }
+        }
+        return result;
+    }
+}
---
+++ test/ru/job4j/lambda/task249967/task249968/PhoneDictionaryTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.lambda.task249967.task249968;
+
+import org.junit.Test;
+import ru.job4j.lambda.task249968.Person;
+import ru.job4j.lambda.task249968.PhoneDictionary;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PhoneDictionaryTest {
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+}
