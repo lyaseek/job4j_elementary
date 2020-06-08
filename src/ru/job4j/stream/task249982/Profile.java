f6aa8902182d59d2b28b66780bf19a1319c94f62
lyaseek <lyaseek@gmail.com>
2020-06-08 10:34:52 +0200

    2. Список адресов[110062#249982]

--- src/ru/job4j/stream/task249982/Profile.java
+++ src/ru/job4j/stream/task249982/Profile.java
@@ -6,14 +6,15 @@ import java.util.stream.Collectors;
 public class Profile {
     private Address address;
 
-    public static class Address {
-        private String city;
-        private String street;
-        private int home;
-        private int apartment;
+    public Address getAddress() {
+        return address;
+    }
+
+    public Profile(Address address) {
+        this.address = address;
     }
 
     public List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(profile -> profile.address).collect(Collectors.toList());
+        return profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
     }
 }
